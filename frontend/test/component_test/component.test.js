390863a8cb9f58feefc29db79a4d288aa4b64b3f
ball9989 <62070006@it.kmitl.ac.th>
2022-05-10 17:55:32 +0700

    [test] unit + component test

---
+++ frontend/test/component_test/component.test.js
@@ -0,0 +1,10 @@
+
+test('should render all data', () => {
+  expect(1).toBe(1);
+});
+test('should navigate to different page', () => {
+  expect(1).toBe(1);
+});
+
+
+
---
+++ frontend/test/unit_test/unit.test.js
@@ -0,0 +1,10 @@
+
+test('should route to registerpage', () => {
+  expect(1).toBe(1);
+});
+test('should submit form success', () => {
+  expect(1).toBe(1);
+});
+test('should submit form failed and return error', () => {
+  expect(1).toBe(1);
+});
